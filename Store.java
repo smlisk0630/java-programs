637382e85cdb66780e50d8207669115f23be330e
Samantha Lisk <63676970+smlisk0630@users.noreply.github.com>
2020-05-26 17:04:27 -0400

    Add Store.java

---
+++ Store.java
@@ -0,0 +1,13 @@
+public class Store {
+    String productType;
+
+    public Store (String.product) {
+        productType = product;
+    }
+
+    public static void main (final String[] args) {
+        final Store lemonadeStand = new Store("lemonade");
+
+    System.out.println(lemonadeStand.productType);
+    }
+}
